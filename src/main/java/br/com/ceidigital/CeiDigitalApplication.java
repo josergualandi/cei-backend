965a7d088ca4bb78b0b6f61dd64b7bb7272d7aef
José Gualandi <josergualandi@users.noreply.github.com>
2025-11-01 12:11:03 -0300

    Initial commit  update .gitignore and set Java 21

---
+++ src/main/java/br/com/ceidigital/CeiDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.ceidigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeiDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeiDigitalApplication.class, args);
+	}
+
+}
